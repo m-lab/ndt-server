314729e38cc7b485d7d3813843acc50cfaba78d8
Simone Basso <bassosimone@gmail.com>
2018-08-10 07:38:25 -0400

    Finish simplifying ndt7 client
    Attempt to remove any remaining optional complexity. Slightly
    tweak the code such that we print the current speed on the
    standard output, which helps to estimate whether we're doing
    a good job when downloading from the local host.
    
    Closes #18.

--- cmd/ndt-cloud-client/main.go
+++ cmd/ndt-cloud-client/main.go
@@ -1,46 +1,28 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 
 	"github.com/apex/log"
 	"github.com/m-lab/ndt-cloud/ndt7"
 )
 
-var disableTLS = flag.Bool("disable-tls", false, "Whether to disable TLS")
-var duration = flag.Int("duration", 10, "Desired duration")
 var hostname = flag.String("hostname", "localhost", "Host to connect to")
-var port = flag.String("port", "3001", "Port to connect to")
+var port = flag.String("port", "3010", "Port to connect to")
 var skipTLSVerify = flag.Bool("skip-tls-verify", false, "Skip TLS verify")
 
 func main() {
 	flag.Parse()
-	settings := ndt7.Settings{}
-	settings.Hostname = *hostname
-	settings.InsecureNoTLS = *disableTLS
-	settings.InsecureSkipTLSVerify = *skipTLSVerify
-	settings.Port = *port
-	settings.Duration = *duration
-	clnt := ndt7.NewClient(settings)
-	ch := make(chan interface{}, 1)
-	defer close(ch)
-	sigs := make(chan os.Signal, 1)
-	defer close(sigs)
-	if runtime.GOOS != "windows" {
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-		go func() {
-			<-sigs
-			log.Warn("Got interrupt signal")
-			ch <- false
-			log.Warn("Delivered interrupt signal")
-		}()
+	clnt := ndt7.Client{}
+	clnt.URL.Scheme = "wss"
+	clnt.URL.Host = *hostname + ":" + *port
+	if *skipTLSVerify {
+		config := tls.Config{InsecureSkipVerify: true}
+		clnt.Dialer.TLSClientConfig = &config
 	}
-	err := clnt.Download()
-	if err != nil {
+	if err := clnt.Download(); err != nil {
 		log.WithError(err).Warn("clnt.Download() failed")
 		os.Exit(1)
 	}
