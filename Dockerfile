191750a4d2b576c408393bd1d2de961c154a601b
Peter Boothe <pboothe@google.com>
2019-02-07 17:01:53 -0500

    Another attempt at travis builds

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM golang:1.11 as m-lab/ndt-server-build
+FROM golang:1.11 as ndt-server-build
 ADD . /go/src/github.com/m-lab/ndt-server
 RUN /go/src/github.com/m-lab/ndt-server/build.sh
 
 # Now copy the built image into the minimal base image
 FROM alpine
-COPY --from=m-lab/ndt-server-build /go/bin/ndt-server /
+COPY --from=ndt-server-build /go/bin/ndt-server /
 ADD ./html /html
 WORKDIR /
 ENTRYPOINT ["/ndt-server"]
--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# Script to build ndt-server with the correct `go get` flags.
+# Script to build ndt-server with the correct `go get` flags.  This script
+# should be run inside a container.
 set -ex
+
 TOPDIR=`cd $(dirname $0) && pwd -P`
 cd $TOPDIR
 VERSION=`git describe --tags`
-GOPATH=$(pwd | sed -e "s#/go/.*#/go#")
 go get -v                                                              \
     -tags netgo                                                        \
     -ldflags "-X github.com/m-lab/ndt-server/version.Version=$VERSION"  \
