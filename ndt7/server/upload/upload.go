00b52e150301c58dd7412caa2d223631e82aebf0
Roberto D'Auria <roberto@measurementlab.net>
2019-02-26 16:29:19 +0100

    (WIP) Add DataDir to Handler and go on implementing upload test.

--- ndt7/server/upload/upload.go
+++ ndt7/server/upload/upload.go
@@ -1,9 +1,10 @@
 package upload
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/m-lab/ndt-server/logging"
@@ -11,8 +12,18 @@ import (
 	"github.com/m-lab/ndt-server/ndt7/spec"
 )
 
+const (
+	// defaultTimeout is the default value of the I/O timeout.
+	defaultTimeout = 7 * time.Second
+
+	// defaultDuration is the default duration of a subtest in nanoseconds.
+	defaultDuration = 10 * time.Second
+)
+
+// Handler handles an upload test on the server side.
 type Handler struct {
 	Upgrader websocket.Upgrader
+	DataDir  string
 }
 
 // warnAndClose emits a warning |message| and then closes the HTTP connection
@@ -23,11 +34,18 @@ func warnAndClose(writer http.ResponseWriter, message string) {
 	writer.WriteHeader(http.StatusBadRequest)
 }
 
+// startMeasuring runs the measurement loop. This runs in a separate goroutine
+// and emits Measurement events on the returned channel.
+func startMeasuring(ctx context.Context, request *http.Request, conn *websocket.Conn, dataDir string) chan model.Measurement {
+	dst := make(chan model.Measurement)
+	//go measuringLoop(ctx, request, conn, dataDir, dst)
+	return dst
+}
+
+// ServeHTTP handles the upload subtest.
 func (ul Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	// TODO: factor out this stuff as it's the same for both UL/DL
 	logging.Logger.Debug("Upgrading to WebSockets")
-
-	fmt.Printf("%s\n", request.Header)
 
 	if request.Header.Get("Sec-WebSocket-Protocol") != spec.SecWebSocketProtocol {
 		warnAndClose(writer, "Missing Sec-WebSocket-Protocol in request")
@@ -44,6 +62,30 @@ func (ul Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 
 	defer conn.Close()
 
+	// Read limit is set to the smallest allowed payload size.
+	conn.SetReadLimit(spec.MinMaxMessageSize)
+
+	// Start measuring loop
+	logging.Logger.Debug("Starting measuring goroutine")
+	ctx, cancel := context.WithCancel(request.Context())
+	measurements := startMeasuring(ctx, request, conn, ul.DataDir)
+
+	// Make sure we clean up resources
+	defer func() {
+		logging.Logger.Debug("Closing the WebSocket connection")
+		conn.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(
+			websocket.CloseNormalClosure, ""), time.Now().Add(defaultTimeout))
+
+		// We could leave the context because the measuring goroutine thinks we're
+		// done or because there has been a socket error. In the latter case, it is
+		// important to synchronise with the goroutine and wait for it to exit.
+		cancel()
+		for range measurements {
+			// NOTHING
+		}
+	}()
+
+	logging.Logger.Debug("Starting receiving data from the client")
 	for {
 		mt, message, err := conn.ReadMessage()
 		if err != nil {
@@ -60,8 +102,7 @@ func (ul Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 			if err == nil {
 				logging.Logger.Errorf("Unable to unmarshal JSON message: %s", message)
 			}
-			logging.Logger.Debugf("Received Measurement - AppInfo.NumBytes: %f", mdata.AppInfo.NumBytes)
+			logging.Logger.Debugf("Received Measurement - AppInfo.NumBytes: %d", mdata.AppInfo.NumBytes)
 		}
-
 	}
 }
