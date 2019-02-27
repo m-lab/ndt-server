e9f125455b2fd61197e08668f326716bf673ed5d
Roberto D'Auria <roberto@measurementlab.net>
2019-02-27 14:26:37 +0100

    Implement measurementLoop in ndt7's upload test.

--- ndt7/server/upload/upload.go
+++ ndt7/server/upload/upload.go
@@ -3,13 +3,20 @@ package upload
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/m-lab/go/iox"
+	"github.com/m-lab/ndt-server/bbr"
+	"github.com/m-lab/ndt-server/fdcache"
 	"github.com/m-lab/ndt-server/logging"
 	"github.com/m-lab/ndt-server/ndt7/model"
+	"github.com/m-lab/ndt-server/ndt7/server/results"
 	"github.com/m-lab/ndt-server/ndt7/spec"
+	"github.com/m-lab/ndt-server/tcpinfox"
 )
 
 const (
@@ -34,11 +41,109 @@ func warnAndClose(writer http.ResponseWriter, message string) {
 	writer.WriteHeader(http.StatusBadRequest)
 }
 
+// getConnFileAndPossiblyEnableBBR returns the connection to be used to
+// gather low level stats and possibly enables BBR. It returns a file to
+// use to gather BBR/TCP_INFO stats on success, an error on failure.
+func getConnFileAndPossiblyEnableBBR(conn *websocket.Conn) (*os.File, error) {
+	fp := fdcache.GetAndForgetFile(conn.UnderlyingConn())
+	// Implementation note: in theory fp SHOULD always be non-nil because
+	// now we always register the fp bound to a net.TCPConn. However, in
+	// some weird cases it MAY happen that the cache pruning mechanism will
+	// remove the fp BEFORE we can steal it. In case we cannot get a file
+	// we just abort the test, as this should not happen (TM).
+	if fp == nil {
+		err := errors.New("cannot get file bound to websocket conn")
+		logging.Logger.WithError(err).Warn("Cannot enable BBR")
+		return nil, err
+	}
+	err := bbr.Enable(fp)
+	if err != nil {
+		logging.Logger.WithError(err).Warn("Cannot enable BBR")
+		// FALLTHROUGH
+	}
+	return fp, nil
+}
+
+// gatherAndSaveTCPInfoAndBBRInfo gathers TCP info and BBR measurements from
+// |sockfp| and stores them into the |measurement| object as well as into the
+// |resultfp| file. Returns an error on failure and nil in case of success.
+func gatherAndSaveTCPInfoAndBBRInfo(measurement *model.Measurement, sockfp *os.File, resultfp *results.File) error {
+	bbrinfo, err := bbr.GetMaxBandwidthAndMinRTT(sockfp)
+	if err == nil {
+		measurement.BBRInfo = &bbrinfo
+	}
+
+	metrics, err := tcpinfox.GetTCPInfo(sockfp)
+	if err == nil {
+		measurement.TCPInfo = &metrics
+	}
+
+	if err := resultfp.WriteMeasurement(*measurement, "server"); err != nil {
+		logging.Logger.WithError(err).Warn("Cannot save measurement on disk")
+		return err
+	}
+	return nil
+}
+
+// measuringLoop is the loop that runs the measurements in a goroutine.
+// This function exits when
+//     (1) a fatal error occurs or
+//     (2) the maximum elapsed time for the upload test expires.
+// Since this function has access to BBR stats (if available), it's the
+// right place to stop the test early. The rest of the upload code is supposed
+// to stop receiving when this function signals that we're done by closing
+// the channel.
+func measuringLoop(ctx context.Context, request *http.Request,
+	conn *websocket.Conn, dataDir string, dst chan model.Measurement) {
+	logging.Logger.Debug("Starting measurement loop")
+	defer logging.Logger.Debug("Stopping measurement loop")
+	defer close(dst)
+
+	resultfp, err := results.OpenFor(request, conn, dataDir, "upload")
+	if err != nil {
+		return // error printed already
+	}
+
+	defer iox.ErrorLoggingCloser(resultfp).Close()
+
+	sockfp, err := getConnFileAndPossiblyEnableBBR(conn)
+	if err != nil {
+		return // error printed already
+	}
+
+	defer iox.ErrorLoggingCloser(sockfp).Close()
+	t0 := time.Now()
+	ticker := time.NewTicker(spec.MinMeasurementInterval)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case now := <-ticker.C:
+			elapsed := now.Sub(t0)
+			if elapsed > defaultDuration {
+				logging.Logger.Debug("Upload has run for long enough.")
+				return
+			}
+
+			measurement := model.Measurement{
+				Elapsed: elapsed.Seconds(),
+			}
+
+			err = gatherAndSaveTCPInfoAndBBRInfo(&measurement, sockfp, resultfp)
+			if err != nil {
+				return // error printed already
+			}
+
+			dst <- measurement
+		}
+	}
+}
+
 // startMeasuring runs the measurement loop. This runs in a separate goroutine
 // and emits Measurement events on the returned channel.
 func startMeasuring(ctx context.Context, request *http.Request, conn *websocket.Conn, dataDir string) chan model.Measurement {
 	dst := make(chan model.Measurement)
-	//go measuringLoop(ctx, request, conn, dataDir, dst)
+	go measuringLoop(ctx, request, conn, dataDir, dst)
 	return dst
 }
 
@@ -87,22 +192,29 @@ func (ul Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 
 	logging.Logger.Debug("Starting receiving data from the client")
 	for {
-		mt, message, err := conn.ReadMessage()
-		if err != nil {
-			if !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				logging.Logger.WithError(err)
+		select {
+		case _, ok := <-measurements:
+			if !ok {
+				return // the goroutine told us it's time to stop running
 			}
-			logging.Logger.Debug(request.RemoteAddr + ": connection closed.")
-			break
-		}
+		default:
+			mt, message, err := conn.ReadMessage()
+			if err != nil {
+				if !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+					logging.Logger.WithError(err)
+				}
+				logging.Logger.Debug(request.RemoteAddr + ": connection closed.")
+				break
+			}
 
-		if mt == websocket.TextMessage {
-			var mdata model.Measurement
-			err := json.Unmarshal(message, &mdata)
-			if err == nil {
-				logging.Logger.Errorf("Unable to unmarshal JSON message: %s", message)
+			if mt == websocket.TextMessage {
+				var mdata model.Measurement
+				err := json.Unmarshal(message, &mdata)
+				if err == nil {
+					logging.Logger.Errorf("Unable to unmarshal JSON message: %s", message)
+				}
+				logging.Logger.Debugf("Received Measurement - AppInfo.NumBytes: %d", mdata.AppInfo.NumBytes)
 			}
-			logging.Logger.Debugf("Received Measurement - AppInfo.NumBytes: %d", mdata.AppInfo.NumBytes)
 		}
 	}
 }
