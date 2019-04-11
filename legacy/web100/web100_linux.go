890c971c81c2fe451d0907d2b6fb9797bd9f01fb
Simone Basso <bassosimone@gmail.com>
2019-04-11 17:48:36 +0200

    Make sure we can build ndt-server on macOS
    We don't support macOS officially. However it's useful to build on
    a system that is different from the containers where we deploy.
    
    As mentioned in #100, I am convinced that Travis is different enough
    from the containers where we deploy to look like macOS.

---
+++ legacy/web100/web100_linux.go
@@ -0,0 +1,62 @@
+package web100
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/sys/unix"
+)
+
+func summarize(snaps []*unix.TCPInfo) (*Metrics, error) {
+	if len(snaps) == 0 {
+		return nil, errors.New("zero-length list of data collected")
+	}
+	minrtt := uint32(0)
+	for _, snap := range snaps {
+		if snap.Rtt < minrtt || minrtt == 0 {
+			minrtt = snap.Rtt
+		}
+	}
+	info := &Metrics{MinRTT: minrtt / 1000} // Convert microseconds to milliseconds.
+	log.Println("Summarized data:", info)
+	return info, nil
+}
+
+// MeasureViaPolling collects all required data by polling. It is required for
+// non-BBR connections because MinRTT is one of our critical metrics.
+func MeasureViaPolling(ctx context.Context, fp *os.File, c chan *Metrics) {
+	defer close(c)
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+	snaps := make([]*unix.TCPInfo, 0, 100)
+	// Poll until the context is canceled.
+	for {
+		// Get the tcp_cc metrics
+		info, err := unix.GetsockoptTCPInfo(int(fp.Fd()), unix.IPPROTO_TCP, unix.TCP_INFO)
+		if err == nil {
+			snaps = append(snaps, info)
+		} else {
+			log.Println("Getsockopt error:", err)
+		}
+		select {
+		case <-ticker.C:
+			continue
+		case <-ctx.Done():
+			info, err := summarize(snaps)
+			if err == nil {
+				c <- info
+			}
+			return
+		}
+	}
+}
+
+// TODO: Implement BBR support for legacy clients.
+/*
+func MeasureBBR(ctx context.Context, fp *os.File) (Metrics, error) {
+	return Metrics{}, errors.New("MeasureBBR is unimplemented")
+}
+*/
