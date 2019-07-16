fb40b4a976aa31e826569148c1794fb44007cc16
Stephen Soltesz <soltesz@google.com>
2019-07-16 15:20:27 -0400

    Simplify the client metadata collection -- eliminate server values

--- ndt7/results/metadata.go
+++ ndt7/results/metadata.go
@@ -4,8 +4,6 @@ package results
 import (
 	"net/url"
 	"regexp"
-
-	"github.com/m-lab/ndt-server/version"
 )
 
 // metadata contains ndt7 metadata.
@@ -14,23 +12,13 @@ type metadata map[string]string
 // serverKeyRe is a regexp that matches any server related key.
 var serverKeyRe = regexp.MustCompile("^server_")
 
-// initMetadata initializes |*meta| from |localEpnt|, |remoteEpnt|, the |values|
-// provided using the query string, and the |subtest| name. Returns an error
-// on failure.
-func initMetadata(m *metadata, localEpnt, remoteEpnt, uuid string, values url.Values, subtest string) {
+// initMetadata initializes |*meta| from |values| provided from the original
+// request query string.
+func initMetadata(m *metadata, values url.Values) {
 	for k, v := range values {
 		if matches := serverKeyRe.MatchString(k); matches {
 			continue // We MUST skip variables reserved to the server
 		}
-		if len(v) != 1 {
-			continue // We SHOULD ignore duplicate values?
-		}
 		(*m)[k] = v[0]
 	}
-	(*m)["server_name"] = "ndt-server"
-	(*m)["server_version"] = version.Version
-	(*m)["server_local_endpoint"] = localEpnt
-	(*m)["server_remote_endpoint"] = remoteEpnt
-	(*m)["server_subtest"] = subtest
-	(*m)["socket_uuid"] = uuid
 }
