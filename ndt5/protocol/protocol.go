c77c5efeae84b4d56313c6eb9598aee77408b93c
Stephen Soltesz <soltesz@google.com>
2019-08-05 16:58:12 -0400

    Move fd close to StartMeasuring

--- ndt5/protocol/protocol.go
+++ ndt5/protocol/protocol.go
@@ -133,11 +133,16 @@ type measurer struct {
 	cancelMeasurementContext context.CancelFunc
 }
 
+// StartMeasuring starts a polling measurement goroutine that runs until the ctx
+// expires. After measurement is complete, the given `fd` is closed.
 func (m *measurer) StartMeasuring(ctx context.Context, fd *os.File) {
 	m.measurements = make(chan *web100.Metrics)
 	var newctx context.Context
 	newctx, m.cancelMeasurementContext = context.WithCancel(ctx)
-	go web100.MeasureViaPolling(newctx, fd, m.measurements)
+	go func() {
+		defer fd.Close()
+		web100.MeasureViaPolling(newctx, fd, m.measurements)
+	}()
 }
 
 func (m *measurer) StopMeasuring() (*web100.Metrics, error) {
