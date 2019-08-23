922f45c71039c296dbfffcbda9585b8b79d012fb
Peter Boothe <pboothe@google.com>
2019-08-23 15:45:24 -0400

    Added comments and explained constants.

--- ndt5/protocol/messager_test.go
+++ ndt5/protocol/messager_test.go
@@ -48,13 +48,20 @@ func TestSendMetrics(t *testing.T) {
 	if err != nil {
 		t.Error("Error should be nil", err)
 	}
-	if len(fm.sentMessages) < 20 {
-		t.Error("Bad messages:", fm)
+	// 73 was chosen because we needed a number that was greater than zero and not
+	// greater than the number of fields in the Metrics struct. This is a moving
+	// target, so we don't want to be too specific and require equality with the
+	// current count. There were a total of 73 fields as of 2019-08-23, so that's a
+	// good lower bound.
+	if len(fm.sentMessages) < 73 {
+		t.Error("Bad messages:", len(fm.sentMessages), fm)
 	}
 }
 
 func TestSendMetricsWithErrors(t *testing.T) {
 	data := &web100.Metrics{}
+	// Erroring after 25 fields means that the error occurs inside the tcpinfo
+	// struct, which exercises both error cases in the recursive function.
 	fm := &fakeMessager{
 		errorAfter: 25,
 	}
@@ -62,7 +69,7 @@ func TestSendMetricsWithErrors(t *testing.T) {
 	if err == nil {
 		t.Error("Error should not be nil", err)
 	}
-	if len(fm.sentMessages) > 30 {
-		t.Error("Bad messages:", fm)
+	if len(fm.sentMessages) > 25 {
+		t.Error("Too many messages sent:", fm)
 	}
 }
