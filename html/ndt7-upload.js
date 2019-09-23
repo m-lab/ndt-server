cfae58430705796656f1404e8de793357c76c7bd
Simone Basso <bassosimone@gmail.com>
2019-09-23 23:42:42 +0000

    Rewrite reference JavaScript client
    The rewritten client:
    
    1. always terminates within the expected timeout
    
    2. is significantly simpler
    
    3. implements binary message scaling
    
    4. implements sleeping when uploading

---
+++ html/ndt7-upload.js
@@ -0,0 +1,54 @@
+/* jshint esversion: 6, asi: true, worker: true */
+// WebWorker that runs the ndt7 upload test
+onmessage = function (ev) {
+  'use strict'
+  let url = new URL(ev.data.href)
+  url.protocol = (url.protocol === 'https:') ? 'wss:' : 'ws:'
+  const wsproto = 'net.measurementlab.ndt.v7'
+  url.pathname = '/ndt/v7/upload'
+  const sock = new WebSocket(url.toString(), wsproto)
+  sock.onclose = function () {
+    postMessage(null)
+  }
+  function uploader(socket, data, start, previous, total) {
+    let now = new Date().getTime()
+    const duration = 10000  // millisecond
+    if (now - start > duration) {
+      sock.close()
+      return
+    }
+    const maxMessageSize = 16777216 /* (1<<24) */
+    if (data.length < maxMessageSize && data.length < (total - sock.bufferedAmount)/16) {
+      data = new Uint8Array(data.length * 2) // TODO(bassosimone): fill this message
+    }
+    const underbuffered = 7 * data.length
+    while (sock.bufferedAmount < underbuffered) {
+      sock.send(data)
+      total += data.length
+    }
+    const every = 250  // millisecond
+    if (now - previous > every) {
+      postMessage({
+        'AppInfo': {
+          'ElapsedTime': (now - start) * 1000,  // us
+          'NumBytes': (total - sock.bufferedAmount),
+        },
+        'Origin': 'client',
+        'Test': 'upload',
+      })
+      previous = now
+    }
+    const drainSpeed = (total - sock.bufferedAmount) / (now - start)
+    const nextSleep = (sock.bufferedAmount / drainSpeed) / 2
+    setTimeout(function() {
+      uploader(sock, data, start, previous, total)
+    }, nextSleep)
+  }
+  sock.onopen = function () {
+    const initialMessageSize = 8192 /* (1<<13) */
+    const data = new Uint8Array(initialMessageSize) // TODO(bassosimone): fill this message
+    sock.binarytype = 'arraybuffer'
+    const start = new Date().getTime()
+    uploader(sock, data, start, start, 0)
+  }
+}
