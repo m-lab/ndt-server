1afccf280323764aa456594d9270b74d61590cf5
Stephen Soltesz <soltesz@google.com>
2020-02-04 17:45:20 +0000

    Cover a proc read failure condition in Watch tests

--- access/tx_test.go
+++ access/tx_test.go
@@ -8,6 +8,9 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
+
+	"github.com/m-lab/go/rtx"
+	"github.com/prometheus/procfs"
 )
 
 func TestTxController_Limit(t *testing.T) {
@@ -103,7 +106,7 @@ func TestTxController_Watch(t *testing.T) {
 		rate     uint64
 		want     *TxController
 		procPath string
-		wantErr  bool
+		badProc string
 		wantWatchErr  bool
 	}{
 		{
@@ -117,19 +120,32 @@ func TestTxController_Watch(t *testing.T) {
 			rate: 1,
 			wantWatchErr: true,
 		},
+		{
+			name:     "success-error-reading-proc",
+			procPath: "testdata/proc-success",
+			rate: 1,
+			badProc: "testdata/proc-nodevfile",
+			wantWatchErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			procPath = tt.procPath
-			got, err := NewTxController(tt.rate)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewTxController() error = %v, wantErr %v", err, tt.wantErr)
+			tx, err := NewTxController(tt.rate)
+			if err != nil {
+				t.Errorf("NewTxController() error = %v, want nil", err)
 				return
 			}
-			got.period = time.Millisecond
+			if tt.badProc != "" {
+				pfs, err := procfs.NewFS(tt.badProc)
+				rtx.Must(err, "Failed to allocate procfs for %q", tt.badProc)
+				// New used a good path, but we replace the pfs with a bad proc record.
+				tx.pfs = pfs
+			}
+			tx.period = time.Millisecond
 			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 			defer cancel()
-			err = got.Watch(ctx)
+			err = tx.Watch(ctx)
 			if (err != nil) != tt.wantWatchErr {
 				t.Errorf("Watch() error = %v, wantErr %v", err, tt.wantWatchErr)
 				return
