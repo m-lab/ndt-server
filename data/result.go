9460dbde5ee3456ddb9b09663254cf878290d4c6
Stephen Soltesz <soltesz@google.com>
2020-02-19 10:30:39 -0500

    Add separate structs for ndt5 & ndt7 datatypes

--- data/result.go
+++ data/result.go
@@ -10,7 +10,7 @@ import (
 	"github.com/m-lab/ndt-server/ndt7/model"
 )
 
-// NDTResult is the struct that is serialized as JSON to disk as the archival
+// NDT5Result is the struct that is serialized as JSON to disk as the archival
 // record of an NDT test.
 //
 // This struct is dual-purpose. It contains the necessary data to allow joining
@@ -21,7 +21,7 @@ import (
 // WARNING: The BigQuery schema is inferred directly from this structure. To
 // preserve compatibility with historical data, never remove fields.
 // For more information see: https://github.com/m-lab/etl/issues/719
-type NDTResult struct {
+type NDT5Result struct {
 	// GitShortCommit is the Git commit (short form) of the running server code.
 	GitShortCommit string
 	// Version is the symbolic version (if any) of the running server code.
@@ -41,6 +41,25 @@ type NDTResult struct {
 	Control *control.ArchivalData `json:",omitempty"`
 	C2S     *c2s.ArchivalData     `json:",omitempty"`
 	S2C     *s2c.ArchivalData     `json:",omitempty"`
+}
+
+// NDT7Result is the struct that is serialized as JSON to disk as the archival
+// record of an NDT7 test. This is similar to, but independent from, the NDTResult.
+type NDT7Result struct {
+	// GitShortCommit is the Git commit (short form) of the running server code.
+	GitShortCommit string
+	// Version is the symbolic version (if any) of the running server code.
+	Version string
+
+	// All data members should all be self-describing. In the event of confusion,
+	// rename them to add clarity rather than adding a comment.
+	ServerIP   string
+	ServerPort int
+	ClientIP   string
+	ClientPort int
+
+	StartTime time.Time
+	EndTime   time.Time
 
 	// ndt7
 	Upload   *model.ArchivalData `json:",omitempty"`
