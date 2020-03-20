65252a11fb12f79f90df2911e4d04ba7832ad03d
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-03-20 19:18:59 -0400

    Remove tiny separate files

--- ndt7/model/appinfo.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// AppInfo contains an application level measurement. This structure is
-// described in the ndt7 specification.
-type AppInfo struct {
-	NumBytes    int64
-	ElapsedTime int64
-}
