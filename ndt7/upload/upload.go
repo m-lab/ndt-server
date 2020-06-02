8c5f1e3fac198d2b7df5733bf835c55cc7f12966
Stephen Soltesz <soltesz@google.com>
2020-06-02 11:20:37 -0400

    Save results to given data

--- ndt7/upload/upload.go
+++ ndt7/upload/upload.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/m-lab/ndt-server/ndt7/measurer"
+	"github.com/m-lab/ndt-server/ndt7/model"
 	"github.com/m-lab/ndt-server/ndt7/receiver"
-	"github.com/m-lab/ndt-server/ndt7/results"
 	"github.com/m-lab/ndt-server/ndt7/saver"
 	"github.com/m-lab/ndt-server/ndt7/upload/sender"
 )
@@ -16,13 +16,13 @@ import (
 // for the subtest. The conn argument is the open WebSocket connection. The
 // resultfp argument is the file where to save results. Both arguments are
 // owned by the caller of this function.
-func Do(ctx context.Context, conn *websocket.Conn, resultfp *results.File) {
+func Do(ctx context.Context, conn *websocket.Conn, data *model.ArchivalData) {
 	// Implementation note: use child context so that, if we cannot save the
 	// results in the loop below, we terminate the goroutines early
 	wholectx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	measurer := measurer.New(conn, resultfp.Data.UUID)
+	measurer := measurer.New(conn, data.UUID)
 	senderch := sender.Start(conn, measurer.Start(ctx))
 	receiverch := receiver.StartUploadReceiver(wholectx, conn)
-	saver.SaveAll(resultfp, senderch, receiverch)
+	saver.SaveAll(data, senderch, receiverch)
 }
