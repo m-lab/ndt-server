bf4841a7c26eebeabad0d6fd2ebaa041dacebca6
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-09-09 15:38:56 -0400

    Simplify

--- ndt7test/ndt7test_test.go
+++ ndt7test/ndt7test_test.go
@@ -15,35 +15,34 @@ import (
 )
 
 func TestNewNDT7Server(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		h, srv := NewNDT7Server(t)
-		defer os.RemoveAll(h.DataDir)
+	// Create the ndt7test server.
+	h, srv := NewNDT7Server(t)
+	defer os.RemoveAll(h.DataDir)
 
-		URL, _ := url.Parse(srv.URL)
-		URL.Scheme = "ws"
-		URL.Path = spec.DownloadURLPath
-		headers := http.Header{}
-		headers.Add("Sec-WebSocket-Protocol", spec.SecWebSocketProtocol)
-		headers.Add("User-Agent", "fake-user-agent")
-		ctx := context.Background()
-		dialer := websocket.Dialer{HandshakeTimeout: 10 * time.Second}
-		conn, _, err := dialer.DialContext(ctx, URL.String(), headers)
-		testingx.Must(t, err, "failed to dial websocket ndt7 test")
-		err = simpleDownload(ctx, t, conn)
-		if err != nil && !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-			testingx.Must(t, err, "failed to download")
-		}
+	// Prepare to run a simplified download with ndt7test server.
+	URL, _ := url.Parse(srv.URL)
+	URL.Scheme = "ws"
+	URL.Path = spec.DownloadURLPath
+	headers := http.Header{}
+	headers.Add("Sec-WebSocket-Protocol", spec.SecWebSocketProtocol)
+	headers.Add("User-Agent", "fake-user-agent")
+	ctx := context.Background()
+	dialer := websocket.Dialer{HandshakeTimeout: 10 * time.Second}
+	conn, _, err := dialer.DialContext(ctx, URL.String(), headers)
+	testingx.Must(t, err, "failed to dial websocket ndt7 test")
+	err = simpleDownload(ctx, t, conn)
+	if err != nil && !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+		testingx.Must(t, err, "failed to download")
+	}
 
-		// Allow the server time to save the file, the client may stop before the server does.
-		time.Sleep(1 * time.Second)
-		// Verify a file was saved.
-		m, err := filepath.Glob(h.DataDir + "/ndt7/*/*/*/*")
-		testingx.Must(t, err, "failed to glob datadir: %s", h.DataDir)
-		if len(m) == 0 {
-			t.Errorf("no files found")
-		}
-
-	})
+	// Allow the server time to save the file, the client may stop before the server does.
+	time.Sleep(1 * time.Second)
+	// Verify a file was saved.
+	m, err := filepath.Glob(h.DataDir + "/ndt7/*/*/*/*")
+	testingx.Must(t, err, "failed to glob datadir: %s", h.DataDir)
+	if len(m) == 0 {
+		t.Errorf("no files found")
+	}
 }
 
 func simpleDownload(ctx context.Context, t *testing.T, conn *websocket.Conn) error {
