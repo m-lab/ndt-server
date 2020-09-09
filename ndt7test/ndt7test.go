c6c0b2193e4fb53a7a21229488375f88c5c24678
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-09-09 14:54:25 -0400

    Simplify setup

--- ndt7test/ndt7test.go
+++ ndt7test/ndt7test.go
@@ -1,120 +1,41 @@
 package ndt7test
 
 import (
-	"context"
-	"flag"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"net/http/httptest"
+	"testing"
 
-	"github.com/m-lab/go/flagx"
+	"github.com/m-lab/go/testingx"
 	"github.com/m-lab/ndt-server/ndt7/handler"
 	"github.com/m-lab/ndt-server/ndt7/spec"
-	"github.com/stephen-soltesz/prometheus-support/golang/go-386/src/net/http/httptest"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/m-lab/ndt-server/netx"
 )
 
-var (
-	// Flags that can be passed in on the command line
-	ndt7Addr          = flag.String("ndt7_addr", ":443", "The address and port to use for the ndt7 test")
-	ndt7AddrCleartext = flag.String("ndt7_addr_cleartext", ":80", "The address and port to use for the ndt7 cleartext test")
-	ndt5Addr          = flag.String("ndt5_addr", ":3001", "The address and port to use for the unencrypted ndt5 test")
-	ndt5WsAddr        = flag.String("ndt5_ws_addr", "127.0.0.1:3002", "The address and port to use for the ndt5 WS test")
-	ndt5WssAddr       = flag.String("ndt5_wss_addr", ":3010", "The address and port to use for the ndt5 WSS test")
-	certFile          = flag.String("cert", "", "The file with server certificates in PEM format.")
-	keyFile           = flag.String("key", "", "The file with server key in PEM format.")
-	dataDir           = flag.String("datadir", "/var/spool/ndt", "The directory in which to write data files")
-	htmlDir           = flag.String("htmldir", "html", "The directory from which to serve static web content.")
-	tokenVerifyKey    = flagx.FileBytesArray{}
-	tokenRequired5    bool
-	tokenRequired7    bool
-	tokenMachine      string
-
-	// A metric to use to signal that the server is in lame duck mode.
-	lameDuck = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "lame_duck_experiment",
-		Help: "Indicates when the server is in lame duck",
-	})
-
-	// Context for the whole program.
-	ctx, cancel = context.WithCancel(context.Background())
-)
-
-func init() {
-	flag.Var(&tokenVerifyKey, "token.verify-key", "Public key for verifying access tokens")
-	flag.BoolVar(&tokenRequired5, "ndt5.token.required", false, "Require access token in NDT5 requests")
-	flag.BoolVar(&tokenRequired7, "ndt7.token.required", false, "Require access token in NDT7 requests")
-	flag.StringVar(&tokenMachine, "token.machine", "", "Use given machine name to verify token claims")
-}
-
-func catchSigterm() {
-	// Disable lame duck status.
-	lameDuck.Set(0)
-
-	// Register channel to receive SIGTERM events.
-	c := make(chan os.Signal, 1)
-	defer close(c)
-	signal.Notify(c, syscall.SIGTERM)
-
-	// Wait until we receive a SIGTERM or the context is canceled.
-	select {
-	case <-c:
-		fmt.Println("Received SIGTERM")
-	case <-ctx.Done():
-		fmt.Println("Canceled")
-	}
-	// Set lame duck status. This will remain set until exit.
-	lameDuck.Set(1)
-	// When we receive a second SIGTERM, cancel the context and shut everything
-	// down. This should cause main() to exit cleanly.
-	select {
-	case <-c:
-		fmt.Println("Received SIGTERM")
-		cancel()
-	case <-ctx.Done():
-		fmt.Println("Canceled")
-	}
-}
-
-func init() {
-	log.SetFlags(log.LUTC | log.LstdFlags | log.Lshortfile)
-}
-
-// httpServer creates a new *http.Server with explicit Read and Write timeouts.
-func httpServer(addr string, handler http.Handler) *http.Server {
-	return &http.Server{
-		Addr:    addr,
-		Handler: handler,
-		// NOTE: set absolute read and write timeouts for server connections.
-		// This prevents clients, or middleboxes, from opening a connection and
-		// holding it open indefinitely. This applies equally to TLS and non-TLS
-		// servers.
-		ReadTimeout:  time.Minute,
-		WriteTimeout: time.Minute,
-	}
-}
-
-func NewNDT7Server() *httptest.Server {
+// NewNDT7Server creates a local httptest server capable of running an ndt7
+// measurement in unittests.
+func NewNDT7Server(t *testing.T) (*handler.Handler, *httptest.Server) {
+	dir, err := ioutil.TempDir("", "ndt7test-*")
+	testingx.Must(t, err, "failed to create temp dir")
 
 	// TODO: add support for token verifiers.
+	// TODO: add support for TLS server.
+	ndt7Handler := &handler.Handler{DataDir: dir}
 	ndt7Mux := http.NewServeMux()
-	ndt7Mux.Handle("/", http.FileServer(http.Dir(*htmlDir)))
-	ndt7Handler := &handler.Handler{
-		DataDir: "/tmp/", // TODO: make dynamic.
-		// SecurePort:   *ndt7Addr, // TODO: support TLS.
-		InsecurePort: ":8888", // TODO: make this dynamic
-	}
 	ndt7Mux.Handle(spec.DownloadURLPath, http.HandlerFunc(ndt7Handler.Download))
 	ndt7Mux.Handle(spec.UploadURLPath, http.HandlerFunc(ndt7Handler.Upload))
+
+	// Create unstarted so we can setup a custom netx.Listener.
 	ts := httptest.NewUnstartedServer(ndt7Mux)
-	log.Println("About to listen for ndt7 cleartext tests on " + ts.URL)
-	// TODO: Update ports.
+	listener, err := net.Listen("tcp", ":0")
+	testingx.Must(t, err, "failed to allocate a listening tcp socket")
+	addr := (listener.(*net.TCPListener)).Addr().(*net.TCPAddr)
+	// Populate insecure port value with dynamic port.
+	ndt7Handler.InsecurePort = fmt.Sprintf(":%d", addr.Port)
+	ts.Listener = netx.NewListener(listener.(*net.TCPListener))
+	// Now that the test server has our custom listener, start it.
 	ts.Start()
-	return ts
+	return ndt7Handler, ts
 }
