856cfe075403daa703989c491e391814c100b056
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-14 17:33:47 -0400

    Add (un)compress option for results

--- ndt7/results/file.go
+++ ndt7/results/file.go
@@ -3,6 +3,7 @@ package results
 import (
 	"compress/gzip"
 	"encoding/json"
+	"io"
 	"os"
 	"path"
 	"time"
@@ -13,31 +14,43 @@ import (
 
 // File is the file where we save measurements.
 type File struct {
-	// Writer is the gzip writer instance
-	Writer *gzip.Writer
+	// Writer is the writer for results.
+	Writer io.Writer
 
-	// Fp is the underlying file
-	Fp *os.File
-
-	// UUID is the UUID of this subtest
+	// UUID is the UUID of this subtest.
 	UUID string
+
+	// fp is the underlying writer file.
+	fp *os.File
+
+	// gzip is an optional writer for compressed results.
+	gzip *gzip.Writer
 }
 
 // newFile opens a measurements file in the current working
 // directory on success and returns an error on failure.
-func newFile(datadir, what, uuid string) (*File, error) {
+func newFile(datadir, what, uuid string, compress bool) (*File, error) {
 	timestamp := time.Now().UTC()
 	dir := path.Join(datadir, "ndt7", timestamp.Format("2006/01/02"))
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
 		return nil, err
 	}
-	name := dir + "/ndt7-" + what + "-" + timestamp.Format("20060102T150405.000000000Z") + "." + uuid + ".json.gz"
+	name := dir + "/ndt7-" + what + "-" + timestamp.Format("20060102T150405.000000000Z") + "." + uuid + ".json"
+	if compress {
+		name += ".gz"
+	}
 	// My assumption here is that we have nanosecond precision and hence it's
 	// unlikely to have conflicts. If I'm wrong, O_EXCL will let us know.
 	fp, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
 	if err != nil {
 		return nil, err
+	}
+	if !compress {
+		return &File{
+			Writer: fp,
+			fp:     fp,
+		}, nil
 	}
 	writer, err := gzip.NewWriterLevel(fp, gzip.BestSpeed)
 	if err != nil {
@@ -46,7 +59,8 @@ func newFile(datadir, what, uuid string) (*File, error) {
 	}
 	return &File{
 		Writer: writer,
-		Fp:     fp,
+		fp:     fp,
+		gzip:   writer,
 	}, nil
 }
 
@@ -55,8 +69,8 @@ func newFile(datadir, what, uuid string) (*File, error) {
 // failure. The "datadir" argument specifies the directory on disk to write the
 // data into and the what argument should indicate whether this is a
 // spec.SubtestDownload or a spec.SubtestUpload ndt7 measurement.
-func NewFile(uuid string, datadir string, what spec.SubtestKind) (*File, error) {
-	fp, err := newFile(datadir, string(what), uuid)
+func NewFile(uuid string, datadir string, what spec.SubtestKind, compress bool) (*File, error) {
+	fp, err := newFile(datadir, string(what), uuid, compress)
 	if err != nil {
 		logging.Logger.WithError(err).Warn("newFile failed")
 		return nil, err
@@ -66,12 +80,14 @@ func NewFile(uuid string, datadir string, what spec.SubtestKind) (*File, error) 
 
 // Close closes the measurement file.
 func (fp *File) Close() error {
-	err := fp.Writer.Close()
-	if err != nil {
-		fp.Fp.Close()
-		return err
+	if fp.gzip != nil {
+		err := fp.gzip.Close()
+		if err != nil {
+			fp.fp.Close()
+			return err
+		}
 	}
-	return fp.Fp.Close()
+	return fp.fp.Close()
 }
 
 // WriteResult serializes |result| as JSON.
