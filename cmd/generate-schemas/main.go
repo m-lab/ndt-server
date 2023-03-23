d92cdc1f9087bafbc1ec1945950f243311fe4504
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-03-22 21:33:37 -0400

    Add schema generator

---
+++ cmd/generate-schemas/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+
+	"github.com/m-lab/go/rtx"
+	"github.com/m-lab/ndt-server/data"
+
+	"cloud.google.com/go/bigquery"
+)
+
+var (
+	ndt7schema string
+	ndt5schema string
+)
+
+func init() {
+	flag.StringVar(&ndt7schema, "ndt7", "/var/spool/datatypes/ndt7.json", "filename to write ndt7 schema")
+	flag.StringVar(&ndt5schema, "ndt5", "/var/spool/datatypes/ndt5.json", "filename to write ndt5 schema")
+}
+
+func main() {
+	flag.Parse()
+	// Generate and save ndt7 schema for autoloading.
+	row7 := data.NDT7Result{}
+	sch, err := bigquery.InferSchema(row7)
+	rtx.Must(err, "failed to generate ndt7 schema")
+	b, err := sch.ToJSONFields()
+	rtx.Must(err, "failed to marshal schema")
+	ioutil.WriteFile(ndt7schema, b, 0o644)
+
+	// Generate and save ndt5 schema for autoloading.
+	row5 := data.NDT7Result{}
+	sch, err = bigquery.InferSchema(row5)
+	rtx.Must(err, "failed to generate ndt5 schema")
+	b, err = sch.ToJSONFields()
+	rtx.Must(err, "failed to marshal schema")
+	ioutil.WriteFile(ndt5schema, b, 0o644)
+}
