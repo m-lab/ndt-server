6fdb416499aeef7eeb6eb584d3a24beae5870e5d
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-11-06 12:32:29 -0500

    Add integration test for handler

---
+++ ndt7/handler/handler_integration_test.go
@@ -0,0 +1,84 @@
+// Package handler implements the WebSocket handler for ndt7.
+package handler_test
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/m-lab/go/testingx"
+	"github.com/m-lab/ndt-server/ndt7/ndt7test"
+	"github.com/m-lab/ndt-server/ndt7/spec"
+	"github.com/m-lab/tcp-info/inetdiag"
+)
+
+// fakeServer implements the eventsocket.Server interface for testing the ndt7 handler.
+type fakeServer struct {
+	created int
+	deleted int
+}
+
+func (f *fakeServer) Listen() error               { return nil }
+func (f *fakeServer) Serve(context.Context) error { return nil }
+func (f *fakeServer) FlowCreated(timestamp time.Time, uuid string, sockid inetdiag.SockID) {
+	f.created++
+}
+func (f *fakeServer) FlowDeleted(timestamp time.Time, uuid string) {
+	f.deleted++
+}
+
+func TestHandler_Download(t *testing.T) {
+	t.Run("download flow events", func(t *testing.T) {
+		fs := &fakeServer{}
+		ndt7h, srv := ndt7test.NewNDT7Server(t)
+		defer srv.Close()
+		ndt7h.Events = fs
+		conn, err := simpleConnect(srv.URL)
+		testingx.Must(t, err, "failed to dial websocket ndt7 test")
+		err = downloadHelper(context.Background(), t, conn)
+		if err != nil && !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+			testingx.Must(t, err, "failed to download")
+		}
+		// Wait briefly for connection close event.
+		time.Sleep(time.Second)
+
+		// Both events should occur once.
+		if fs.created == 0 {
+			t.Errorf("flow events created not detected; got %d, want 1", fs.created)
+		}
+		if fs.deleted == 0 {
+			t.Errorf("flow events deleted not detected; got %d, want 1", fs.deleted)
+		}
+	})
+}
+
+func simpleConnect(srv string) (*websocket.Conn, error) {
+	// Prepare to run a simplified download with ndt7test server.
+	URL, _ := url.Parse(srv)
+	URL.Scheme = "ws"
+	URL.Path = spec.DownloadURLPath
+	headers := http.Header{}
+	headers.Add("Sec-WebSocket-Protocol", spec.SecWebSocketProtocol)
+	headers.Add("User-Agent", "fake-user-agent")
+	ctx := context.Background()
+	dialer := websocket.Dialer{HandshakeTimeout: 10 * time.Second}
+	conn, _, err := dialer.DialContext(ctx, URL.String(), headers)
+	return conn, err
+}
+
+// WARNING: this is not a reference client.
+func downloadHelper(ctx context.Context, t *testing.T, conn *websocket.Conn) error {
+	defer conn.Close()
+	conn.SetReadLimit(spec.MaxMessageSize)
+	err := conn.SetReadDeadline(time.Now().Add(spec.MaxRuntime))
+	testingx.Must(t, err, "failed to set read deadline")
+	_, _, err = conn.ReadMessage()
+	if err != nil {
+		return err
+	}
+	// We only read one message, so this is an early close.
+	return conn.Close()
+}
